b56e28a576b1c6c6aadb0c16654b3cd153e03e26
Eitan Ulanovsky <eitan.ula@gmail.com>
2024-02-05 20:22:26 +0200

    version v2.05

--- bot.py
+++ bot.py
@@ -121,9 +121,9 @@ async def on_message(message):
         user = post.get_author()
         auth = post.author
         icon = user.icon_url
-        date = datetime.fromisoformat(post.posted.replace("Z", "+00:00").replace("T", " "))
-        edit = datetime.fromisoformat(post.edited.replace("Z", "+00:00").replace("T", " "))
-
+        date = post.posted.replace("Z", "+00:00").replace("T", " ")
+        edit = post.edited.replace("Z", "+00:00").replace("T", " ")
+        date, edit = datetime.fromisoformat(date), datetime.fromisoformat(edit)
         cont = post.html_content
         pattern = re.compile('<.*?>')
         surl = "https://scratch.mit.edu/discuss/topic/" + id[1]
@@ -258,7 +258,7 @@ async def on_message(message):
                         inline=False)
         embed.add_field(name=">invite", value="The invite link.",
                         inline=False)
-        embed.set_footer(text="VERSION 2.04")
+        embed.set_footer(text="VERSION 2.05")
         await message.channel.send(embed=embed)
 
 bot.run(TOKEN)
