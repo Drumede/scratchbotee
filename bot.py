50ae61324e01fb2b5f97ec27089d0f9eed237aac
Eitan Ulanovsky <eitan.ula@gmail.com>
2024-10-30 20:19:13 +0200

    i hate you

--- bot.py
+++ bot.py
@@ -116,6 +116,7 @@ async def on_message(message):
     def studio(id):
         studio = scratchattach.get_studio(id[1])
         user = studio.host()
+        print(user)
         icon = user.icon_url
         title = html.unescape(studio.title)
         desc = html.unescape(studio.description)
